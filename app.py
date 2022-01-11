0a43cf93992fc7f059171f7ab8df7bae1db089fb
hang <hangxiee@gmail.com>
2022-01-10 21:13:16 -0500

    backend configuration done:

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'testFlake8testFlake8testFlake8testFlake8testFlake8testFlake8testFlake8testFlake8testFlake8testFlake8testFlake8testFlake8testFlake8testFlake8testFlake8testFlake8testFlake8testFlake8testFlake8testFlake8testFlake8testFlake8testFlake8testFlake8testFlake8testFlake8testFlake8testFlake8testFlake8testFlake8testFlake8testFlake8testFlake8testFlake8'
+    return 'HelloWorld'
 
 
 if __name__ == '__main__':
